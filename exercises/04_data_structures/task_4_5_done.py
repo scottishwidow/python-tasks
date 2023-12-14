cbd4fed2b02ad90e2b029034c3b58a4b9e060edb
scottishwidow <mike.voznyanskix@gmail.com>
2023-12-14 18:37:04 +0200

    task 4.5 done

--- exercises/04_data_structures/task_4_5_done.py
+++ exercises/04_data_structures/task_4_5_done.py
@@ -25,3 +25,10 @@ command1 і в команді command2 (перетин). Елементи спи
 
 command1 = "switchport trunk allowed vlan 1,2,3,5,8"
 command2 = "switchport trunk allowed vlan 1,3,8,9"
+
+vlans1 = set(command1.split()[-1].split(','))
+vlans2 = set(command2.split()[-1].split(','))
+
+result = sorted(list(vlans1.intersection(vlans2)))
+
+print(result)
